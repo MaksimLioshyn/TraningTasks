626180f93e35d057b5fab75427da72be9e155ab7
GOMEL\Maksim_Lioshyn <Maksim_Lioshyn@epam.com>
2018-09-18 15:13:19 +0300

    Add tests and first 5 tasks

---
+++ ClassLibraryNumbers/Numbers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ClassLibraryNumbers
+{
+    public static class Numbers
+    {
+        public static string FindPItoTheNthDigit(int n) => Math.PI.ToString(CultureInfo.InvariantCulture).Substring(0, n + 2);
+
+        public static string FindEToTheNthDigit(int n) => Math.E.ToString(CultureInfo.InvariantCulture).Substring(0, n + 2);
+
+        public static long FibonacciSequence(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            long[] l = new long[n];
+            l[0] = 1;
+            l[1] = 1;
+            for (int i = 2; i < n; i++)
+            {
+                l[i] = l[i - 1] + l[i - 2];
+            }
+
+            return l[n - 1];
+        }
+
+        public static List<long> PrimeFactorization(long n)
+        {
+            List<long> l = new List<long> {1};
+            for (long i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    l.Add(i);
+                    n /= i;
+                }
+            }
+
+            return l;
+        }
+
+        public static long NextPrimeNumber(int n)
+        {
+            long l= 1;
+            List<long> list = new List<long>();
+            int i = 0;
+            while (n!=i)
+            {
+                l++;
+                for (int j = 2; j <= l; j++)
+                {
+                    if ((l % j == 0))
+                    {
+                        if (l==j)
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return l;
+        }
+    }
+}
---
+++ ClassLibraryNumbersTests/NumbersTests.cs
@@ -0,0 +1,66 @@
+﻿using ClassLibraryNumbers;
+using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using static ClassLibraryNumbers.Numbers;
+
+namespace ClassLibraryNumbersTests
+{
+    [TestFixture()]
+    public class NumbersTests
+    {
+        [TestCase(1, ExpectedResult = "3.1")]
+        [TestCase(2, ExpectedResult = "3.14")]
+        [TestCase(3, ExpectedResult = "3.141")]
+        [TestCase(4, ExpectedResult = "3.1415")]
+        [TestCase(5, ExpectedResult = "3.14159")]
+        [TestCase(6, ExpectedResult = "3.141592")]
+        public string FindPItoTheNthDigitTest(int n)
+        {
+            return FindPItoTheNthDigit(n);
+        }
+
+        [TestCase(1, ExpectedResult = "2.7")]
+        [TestCase(2, ExpectedResult = "2.71")]
+        [TestCase(3, ExpectedResult = "2.718")]
+        [TestCase(4, ExpectedResult = "2.7182")]
+        [TestCase(5, ExpectedResult = "2.71828")]
+        [TestCase(6, ExpectedResult = "2.718281")]
+        public string FindEToTheNthDigitTest(int n)
+        {
+            return FindEToTheNthDigit(n);
+        }
+
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(3, ExpectedResult = 2)]
+        [TestCase(4, ExpectedResult = 3)]
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(6, ExpectedResult = 8)]
+        [TestCase(13, ExpectedResult = 233)]
+        public long FibonacciSequenceTest(int n)
+        {
+            return FibonacciSequence(n);
+        }
+
+        [TestCase(41, ExpectedResult = new long[] { 1, 41 })]
+        [TestCase(4, ExpectedResult = new long[] { 1, 2, 2 })]
+        [TestCase(2108, ExpectedResult = new long[] { 1, 2, 2, 17, 31 })]
+        [TestCase(6207, ExpectedResult = new long[] { 1, 3, 2069 })]
+        [TestCase(6144, ExpectedResult = new long[] { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3 })]
+        public long[] PrimeFactorizationTest(long n)
+        {
+            return PrimeFactorization(n).ToArray();
+        }
+
+        [TestCase(1, ExpectedResult = 2)]
+        [TestCase(2, ExpectedResult = 3)]
+        [TestCase(3, ExpectedResult = 5)]
+        [TestCase(4, ExpectedResult = 7)]
+        [TestCase(5, ExpectedResult = 11)]
+        [TestCase(6, ExpectedResult = 13)]
+        public long NextPrimeNumberTest(int n)
+        {
+            return NextPrimeNumber(n);
+        }
+    }
+}
